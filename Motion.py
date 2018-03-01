61674884422df7f37e839f9295f86fba487e0a37
YousefMansy <y.o.mansy@gmail.com>
2018-03-02 01:48:09 +0200

    motion

--- Motion.py
+++ Motion.py
@@ -116,7 +116,7 @@ class Motion(Component):
         LeftComponent = resultant * math.sin(alpha ) * maximum_factor
 
         front_right_thruster_value = int(self.MOTORS_BASE_PWM + (LeftComponent * self.FULL_PWM_RANGE_COEFFICIENT))
-        front_left_thruster_value = int(self.MOTORS_BASE_PWM - (RightComponent * self.FULL_PWM_RANGE_COEFFICIENT))
+        front_left_thruster_value = int(self.MOTORS_BASE_PWM + (RightComponent * self.FULL_PWM_RANGE_COEFFICIENT))
 
         back_right_thruster_value = self.PWMNORMAL
         back_left_thruster_value = self.PWMNORMAL
